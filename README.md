5ac1baf7433e82fa60b3f97246e26c5de178aa60
Adrian Rojo <adrj67@gmail.com>
2025-09-02 11:09:26 -0300

    Iconos e Imagenes en Flutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# imagenes_app
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:cloudinary_flutter/image/cld_image.dart';
+import 'package:cloudinary_flutter/cloudinary_object.dart';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+late CloudinaryObject cloudinary;
+
+/*
+LISTA REPRODUCCION
+https://www.youtube.com/playlist?list=PLkVpKYNT_U9frI8-qia9vN3k-V7huavBB
+
+10) https://www.youtube.com/watch?v=EWFcxoNrRGY - Imagenes
+11) https://www.youtube.com/watch?v=eBIWYzktK1U&list=PLkVpKYNT_U9frI8-qia9vN3k-V7huavBB&index=23 - Texto
+minuto 
+      
+*/
+
+/*
+void main() { 
+  cloudinary = CloudinaryObject.fromCloudName(
+    cloudName: dotenv.env['CLOUD_NAME']!
+    );
+
+  runApp(MyApp()); 
+}
+*/
+
+Future<void> main() async {
+
+  // Necesario para inicializaciones antes de runApp()
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: "dev.env");//nombre archivo
+
+  cloudinary = CloudinaryObject.fromCloudName(
+    cloudName: dotenv.env['CLOUD_NAME']!,
+    );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Imagenes App',
+      debugShowCheckedModeBanner: false,//desaparece el cartel 'debug' en el angulo superior derecho.
+      home: MyHomePage(title: 'Imagenes'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+ MyHomePage({Key? key, this.title}):super(key:key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("${widget.title}", 
+        style: TextStyle(color: Colors.white,),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Material Icon: ', 
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Icon(
+                  Icons.home,
+                  color: Colors.deepPurple,
+                  size: 30,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Cupertino Icon: ', 
+                  style: TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Icon(
+                  CupertinoIcons.home,
+                  color: Colors.indigo,
+                  size: 30,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'FontAwesome Icono: ', 
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Icon(
+                  FontAwesomeIcons.house,
+                  color: Colors.blueGrey,
+                  size: 30,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/CocineraColor.jpg',
+                  width: 250,
+                  ),
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(10)),// separa las imagenes
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  // si no levanta la imagen desde la web, paramos el proyecto y en la terminal 
+                  // ponemos " flutter run -d chrome --no-sound-null-safety --web-renderer=html"
+                  // eso renderiza la pagina de la que tomamos la imagen y la carga,
+                  // con el emulador no hace falta
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCBNjE5HAnkdh3TIXkz2PqoE-dkdMgBEMATw&s',
+                  width: 250,
+                  ),
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            // cloudinary
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 250,
+                  height: 140,
+                  child: CldImageWidget(
+                  cloudinary: cloudinary,
+                  publicId: "bzmro1shx1cuavvvek07.jpg",
+                ),
+              ),]
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
